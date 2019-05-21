24bcec24f4cd931d4497f7d7c507a1add1ca8a6b
duc <daihocmodiachat1997@gmail.com>
2019-05-21 19:25:35 +0700

    init

--- index.php
+++ index.php
@@ -7,22 +7,24 @@
 
 </head>
 <body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Tính chỉ số BMI</h1>
 
-<h1>Tính chỉ số BMI</h1>
-
-<form name="bmi" action="" method="post">
-    <div class="form-group">
-        <label >Chiều cao ( cm )</label>
-        <input type="text" class="form-control" name="height" value="">
+            <form name="bmi" action="" method="post">
+                <div class="form-group">
+                    <label >Chiều cao ( cm )</label>
+                    <input type="text" class="form-control" name="height" value="">
+                </div>
+                <div class="form-group">
+                    <label >Cân nặng ( kg ) </label>
+                    <input type="text" class="form-control" name="weight">
+                </div>
+                <button type="submit" name="calc" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-        <label >Cân nặng ( kg ) </label>
-        <input type="text" class="form-control" name="weight">
-    </div>
-    <button type="submit" name="calc" class="btn btn-primary">Submit</button>
-</form>
-
-
-
+</div>
 </body>
 </html>
